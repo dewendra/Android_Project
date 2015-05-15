f542920c79a49e5e52808d3bf2244788ac337d55
dewendra <dewendra1.singh@gmail.com>
2015-05-15 10:21:18 +0530

    my first drag and drop

---
+++ DragAndDrop/src/com/example/draganddrop/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.draganddrop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.divide_screen);
+		
+		findViewById(R.id.imageView1).setOnTouchListener(this);
+		
+		
+		}
+
+	
+}
